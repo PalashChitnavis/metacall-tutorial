8903c847b86bdc5edc3e2bb063e70383c6fe6b00
Palash Chitnavis <palashchitnavis@gmail.com>
2025-02-22 22:33:48 +0530

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs/",
+  baseUrl: "/metacall-tutorial/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "metacall", // Usually your GitHub org/user name.
-  projectName: "doc", // Usually your repo name.
+  organizationName: "PalashChitnavis", // Usually your GitHub org/user name.
+  projectName: "metacall-tutorial", // Usually your repo name.
   deploymentBranch: "gh-pages",
 
   onBrokenLinks: "ignore",
